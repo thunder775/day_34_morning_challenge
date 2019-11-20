b0b5eadd070ed6c03ecdf40e97cd05a27b7ce326
thunder775 <rahulmasih61@gmail.com>
2019-11-20 14:22:21 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -5,6 +5,24 @@
 //  Examples
 //  minRemovals("abcde", "cab") ➞ 2
 
+int minRemoval(String str1, String str2) {
+  var strList1 = List<String>.from(str1.split(''));
+  var strList2 = List<String>.from(str2.split(''));
+  for (int i = 0; i < str1.length; i++) {
+    for (int j = 0; j < str2.length; j++) {
+      if (str1[i] == str2[j]) {
+        strList1.remove(str1[i]);
+        strList2.remove(str2[j]);
+        break;
+      }
+    }
+  }
+  return (strList1.length + strList2.length);
+}
+
+bool isAnagram(String str1, String str2) {
+  return minRemoval(str1, str2) == 0;
+}
 
 // Challenge 2
 // Farthest Hamming Anagrams
@@ -26,6 +44,31 @@
 //  their length (i.e. no letters in the same positions).
 // - false if they aren't anagrams, or
 // - Their Hamming distance if they are anagrams with >=1 letter at the same index.
+dynamic hammingDistance(String str1, String str2) {
+  int hammingDistance = 0;
+  for (int i = 0; i < str1.length; i++) {
+    if (str1[i] != str2[i]) {
+      hammingDistance += 1;
+    }
+  }
+  if (hammingDistance == str1.length && isAnagram(str1, str2)) {
+    return true;
+  }
+  if (!isAnagram(str1, str2)) {
+    return false;
+  }
+  if (hammingDistance != str1.length && isAnagram(str1, str2)) {
+    return hammingDistance;
+  }
+}
 
 main() {
+//  print(minRemoval('balloon', 'balllooon'));
+//  print(minRemoval('met', 'meet'));
+
+  print(minRemoval('mood', 'doom'));
+  print(isAnagram('mood', 'domo'));
+  print(isAnagram('met', 'meet'));
+  print(isAnagram('balloon', 'balllooon'));
+//  print(hammingDistance('listen', 'silent'));
 }
--- test/Day_34_Morning_Challenge_test.dart
+++ test/Day_34_Morning_Challenge_test.dart
@@ -1,4 +1,15 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
+  test('minRemoval', () {
+    expect(minRemoval('root', 'boot'), 2);
+  });
+  test('hamming distance', () {
+    expect(hammingDistance('listen', 'silent'), 5);
+    expect(hammingDistance('read', 'dear'), 2);
+    expect(isAnagram('balloon', 'balllooon'), false);
+    expect(isAnagram('met', 'meet'), false);
+    expect(isAnagram('mood', 'doom'), true);
+  });
 }
